52a4db12c95ca2d3dc690f9b5cb502ff134cdc66
LorensoAEchavarria <Lorensoaechavarria@gmail.com>
2019-11-07 22:51:03 +0000

    finish off the inputs

--- js/script.js
+++ js/script.js
@@ -4,8 +4,15 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+$("#shoot").click(function(){
+ let input =$("#input").val();
+if (input === "rock" || input === "paper" || input === "scissors") {
+$("#userChoice").html(`<p> ${input} </p>`);
+}else{
+    ("#result").append("<p>" + "Please put rock,paper, or scissors" + "</p>")
+}
 
-
+});
 
 // DOCUMENT READY FUNCTION BELOW
 
